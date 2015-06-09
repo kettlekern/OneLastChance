279bfa2784ef9c0fa361229f2411a0f7d691c026
Andrew Kennedy <andrewk36@gmail.com>
2015-06-09 03:15:21 -0700

    Added todo and removed shooting

--- OneLastChance/Assets/Done/Done_Scripts/Done_PlayerController.cs
+++ OneLastChance/Assets/Done/Done_Scripts/Done_PlayerController.cs
@@ -23,12 +23,6 @@ public class Done_PlayerController : MonoBehaviour
 	
 	void Update ()
 	{
-//		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-//		{
-//			nextFire = Time.time + fireRate;
-//			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-//			GetComponent<AudioSource>().Play ();
-//		}
 	}
 
 	void FixedUpdate ()
